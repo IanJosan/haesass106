6dfd4a477d587d05b2c05e15501a128964d99982
Ian <shameless@galage.cn>
2022-09-08 15:46:34 +0800

    响应拦截

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,6 +1,30 @@
+import { getToken, setToken, removeToken } from '@/utils/auth'
+import { login } from '@/api/user'
+// 放置状态
+const state = {
+  token: getToken() // 设置token为共享
+}
+const mutations = {
+  setToken(state, token) {
+    state.token = token
+    setToken(token) // 数据持久化
+  },
+  removeToken(state) {
+    state.token = null
+    removeToken()
+  }
+}
+const actions = {
+  async login(context, data) {
+    const result = await login(data)
+    if (result.data) {
+      context.commit('setToken', result.data.data)
+    }
+  }
+}
 export default {
   namespaced: true,
-  state: {},
-  mutations: {},
-  actions: {}
+  state,
+  mutations,
+  actions
 }
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,12 +1,26 @@
 import axios from 'axios'
-
+import { Message } from 'element-ui'
 // create an axios instance
-const service = axios.create({})
+const service = axios.create({
+  baseURL: process.env.VUE_APP_BASE_API,
+  timeout: 15000
+})
 
 // request interceptor
 service.interceptors.request.use()
 
-// response interceptor
-service.interceptors.response.use()
+// response interceptor 响应拦截器
+service.interceptors.response.use(response => {
+  const { success, message, data } = response.data
+  if (success) {
+    return data
+  } else {
+    Message.error(message)
+    return Promise.reject(new Error(message))
+  }
+}, error => {
+  Message.error(error.message)
+  return Promise.reject(error)
+})
 
 export default service
